3f7555f04efeffb524b3e7a866a27bdac04fe853
minsol1 <81753858+minsol1@users.noreply.github.com>
2024-03-01 22:24:07 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 252 ms, Memory: 19424 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,90 +1,72 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+// 젤다 
 public class Main {
+	static int N;
+	static int[] dx = { 1, 0, -1, 0 };
+	static int[] dy = { 0, 1, 0, -1 };
+	static int[][] arr;
+	static int[][] dist;
 
-	static class Node implements Comparable<Node>{
-		int x;
-		int y;
-		int cost;
-		
-		public Node(int x, int y, int cost) {
-			this.x = x;
-			this.y = y;
-			this.cost = cost;
-			
-		}
+	static void di() {
+		PriorityQueue<int[]> q = new PriorityQueue<int[]>((o1, o2) -> o1[2]-o2[2]);
+		q.add(new int[] { 0, 0 ,arr[0][0]});
+		dist[0][0] = arr[0][0];
 
-		@Override
-		public int compareTo(Node o) {
-			return this.cost - o.cost;
-		}
-	}
-	static int n;
-	static int[][] map;
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		String line = null;
-		int idx=1;
-		while(!(line = br.readLine()).equals("0")) {
-			n = Integer.parseInt(line);
-			map = new int[n][n];
-			
-			StringTokenizer st = null;
-			for(int i=0; i<n; i++) {
-				st = new StringTokenizer(br.readLine());
-				for(int j=0; j<n; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			
-			int cost = bfs();
-			sb.append("Problem " + idx + ": " + cost+"\n");
-			idx++;
-		}
-		System.out.println(sb.toString());
-	}
+		while (!q.isEmpty()) {
+			int[] now = q.poll();
 	
-	static int bfs() {
-		Queue<Node> q = new PriorityQueue<>();
-		int[][] move = new int[n][n];
-		for(int i=0; i<n; i++) {
-			Arrays.fill(move[i], Integer.MAX_VALUE);
-		}
-		
-		q.add(new Node(0, 0, map[0][0]));
-		move[0][0] = map[0][0];
-		
-		while(!q.isEmpty()) {
-			Node pos = q.poll();
-			int px = pos.x, py = pos.y;
-			int cost = pos.cost;
-			
-			if(px == n-1 && py == n-1) {
-				return cost;
+			if(now[0] == N-1 && now[1] == N-1) {
+				return;
 			}
-			
-			for(int i=0; i<4; i++) {
-				int nx = px + dx[i];
-				int ny = py + dy[i];
-				
-				if(nx<0 || nx>n-1 || ny<0 || ny>n-1) continue;
-				
-				if(cost+map[ny][nx] < move[ny][nx]) {
-					move[ny][nx] =cost+map[ny][nx];
-					q.add(new Node(nx,ny,cost+map[ny][nx]));
+
+			for (int d = 0; d < 4; d++) {
+
+				int nx = now[0] + dx[d];
+				int ny = now[1] + dy[d];
+
+				if (nx < 0 || nx >= N || ny < 0 || ny >= N ) {
+					continue;
 				}
-				
+				if (dist[nx][ny] > dist[now[0]][now[1]] + arr[nx][ny]) {
+					dist[nx][ny] = dist[now[0]][now[1]] + arr[nx][ny];
+					q.add(new int[] { nx, ny ,dist[nx][ny]});
+				}
 			}
+
 		}
-		return -1;
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		int t = 1;
+		while (N != 0) {
+			arr = new int[N][N];
+			dist = new int[N][N];
+			
+
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+					dist[i][j] = 10*N*N;
+				}
+			}
+
+			di();
+
+			System.out.printf("Problem %d: %d\n", t, dist[N - 1][N - 1]);
+			
+			t++;
+			N = Integer.parseInt(br.readLine());
+		}
 	}
 }
