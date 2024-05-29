a33ed393afa9137ecc12e141d197b515845f7491
minsol1 <81753858+minsol1@users.noreply.github.com>
2024-05-29 16:12:30 +0900

    [Silver II] Title: N과 M (10), Time: 80 ms, Memory: 11596 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static int N,M, size;
+	public static StringBuilder sb;
+	public static int[] arr = new int[10001];
+	
+	public static void dfs(int idx, int cnt, String temp) {
+		if(cnt==M) {
+			sb.append(temp).append("\n");
+			return;
+		}
+		
+		for(int i = idx; i<size+1;i++) {
+			if(arr[i]>0) {
+				arr[i]--;
+				dfs(i,cnt+1,temp+i+" ");
+				arr[i]++;
+			}
+		}
+	}
+	
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		size =0;
+		
+		sb= new StringBuilder();
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0;i<N;i++) {
+			int n = Integer.parseInt(st.nextToken());
+			arr[n]++;
+			if (n>size) {
+				size=n;
+			}
+		}
+		
+		dfs(0, 0,"");
+		
+		System.out.println(sb);
+
+	}
+
+}
