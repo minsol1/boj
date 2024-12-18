935ae195860d2e09cf902da78bffed9ff1a0ff11
minsol1 <81753858+minsol1@users.noreply.github.com>
2024-12-18 22:41:24 +0900

    [Gold V] Title: 동전 2, Time: 84 ms, Memory: 11856 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,43 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args)  throws IOException {
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N  = Integer.parseInt(st.nextToken());
+        int K  = Integer.parseInt(st.nextToken());
+        int[] dp = new int[K+1];
+        int[] cost = new int[N];
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		int[] dp = new int[K+1];
-		int c;
-		
-		for(int i=1;i<K+1 ; i++) {
-			dp[i] = K+1;
-		}
-		
-		for(int i =0;i<N;i++) {
-			c = Integer.parseInt(br.readLine());
-			if(c>K) {
-				continue;
-			}
-			if(dp[c]==1) {
-				continue;
-			}
-			dp[c] =1;
-			for(int j = c+1;j<K+1;j++) {
-				dp[j] = dp[j]<dp[j-c]+1?dp[j]:dp[j-c]+1;
-			}
-		}
-		
-		if(dp[K]==K+1) {
-			System.out.println(-1);
-		}else {
-			
-			System.out.println(dp[K]);
-		}
-	}
+        for(int i = 1; i<K+1;i++){
+            dp[i] = 10001;
+        }
 
+        for(int i =0; i<N;i++){
+            cost[i] = Integer.parseInt(br.readLine());
+        }
+
+        for(int i =1; i<K+1;i++){
+           for(int j = 0;j<N;j++){
+                if(i>=cost[j]){
+                    dp[i] = dp[i]<dp[i-cost[j]]+1?dp[i]:dp[i-cost[j]]+1;
+                }
+            }
+        }
+
+        if(dp[K]==10001){
+            System.out.print(-1);
+        }else{
+            System.out.print(dp[K]);
+        }
+    }
 }
