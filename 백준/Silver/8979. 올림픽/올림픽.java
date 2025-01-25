f5e652c9896bf249d478822ea50e447942a87da1
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-01-25 14:39:03 +0900

    [Silver V] Title: 올림픽, Time: 64 ms, Memory: 11400 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] arr =new int[N+1][3];
+        int res =1;
+
+        for(int i =0;i<N;i++){
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            arr[n][0] = Integer.parseInt(st.nextToken());
+            arr[n][1] = Integer.parseInt(st.nextToken());
+            arr[n][2] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i =1;i<N+1;i++){
+            if(i == M) continue;
+            if(arr[i][0]< arr[M][0]){
+                res++;
+            }
+            else if(arr[i][0]== arr[M][0] && arr[i][1]<arr[M][1]){
+                res++;
+            }
+            else if(arr[i][0]== arr[M][0] && arr[i][1]==arr[M][1]&& arr[i][2]<arr[M][2]){
+                res++;
+            }
+
+        }
+        System.out.print(res);
+
+    }
+}
