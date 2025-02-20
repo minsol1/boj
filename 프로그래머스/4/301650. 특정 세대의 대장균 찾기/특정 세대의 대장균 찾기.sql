7ff6bed5286f74f79b69e99b19be006e82bf9ce6
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-02-20 10:18:20 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,11 +1,12 @@
 -- 코드를 작성해주세요
 select ID
 from ECOLI_DATA
-where PARENT_ID in (select ed1.ID
-                   from ECOLI_DATA ed1 right join (select ID
-                                                  from ECOLI_DATA
-                                                  where PARENT_ID is null
-                   ) ed2
-                   on ed1.PARENT_ID = ed2.ID
-                   where ed1.ID is not null)
+where PARENT_ID in (select ID
+                   from ECOLI_DATA 
+                    where PARENT_ID in (
+                                        select ID
+                        from ECOLI_DATA 
+                    where PARENT_ID is null
+                    )
+                    )
 order by ID
