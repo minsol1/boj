b1382f5fcda10c7e30f6df4a5d9d0af54baa9753
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-06-27 11:53:17 +0900

    [Silver II] Title: DNA 비밀번호, Time: 184 ms, Memory: 20580 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,108 +1,83 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int S;
-	static int P;
-	static String str;
-	static int res;
-	static int[] arr;
-	static int[] cnt;
-	
-	static void check() {
-		for(int i=0;i<4;i++) {
-			if(arr[i]>cnt[i]) {
-				return;
-			}
-		}
-		res++;
-	}
-	
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static int N,M,res;
+    public static String str;
+    public static int[] arr;
+    public static int[] cnt;
 
-		S = Integer.parseInt(st.nextToken());
-		P = Integer.parseInt(st.nextToken());
-		
-		str = br.readLine();
-		
-		arr = new int[4];
-		cnt = new int[4];
-		res =0;
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i =0;i<4;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		for(int i=0;i<P;i++) {
-			switch(str.charAt(i)) {
-			case 'A':
-				cnt[0]++;
-				break;
-			
-			case 'C':
-				cnt[1]++;
-				break;
-				
-			case 'G':
-				cnt[2]++;
-				break;
-				
-			case 'T':
-				cnt[3]++;
-				break;
-			}
-		}
-		
-		check();
-		
-		for(int i=0;i<S-P;i++) {
-			switch(str.charAt(i)) {
-			case 'A':
-				cnt[0]--;
-				break;
-				
-			case 'C':
-				cnt[1]--;
-				break;
-				
-			case 'G':
-				cnt[2]--;
-				break;
-				
-			case 'T':
-				cnt[3]--;
-				break;
-			}
-			switch(str.charAt(i+P)) {
-			case 'A':
-				cnt[0]++;
-				break;
-				
-			case 'C':
-				cnt[1]++;
-				break;
-				
-			case 'G':
-				cnt[2]++;
-				break;
-				
-			case 'T':
-				cnt[3]++;
-				break;
-			}
-			
-			check();
-		}
-		
-		
-		
-		System.out.println(res);
-	}
+    public static void add(char c){
+        if(c == 'A'){
+            cnt[0]++;
+        }
+        if(c == 'C'){
+            cnt[1]++;
+        }
+        if(c == 'G'){
+            cnt[2]++;
+        }
+        if(c == 'T'){
+            cnt[3]++;
+        }
+    }
+    public static void sub(char c){
+        if(c == 'A'){
+            cnt[0]--;
+        }
+        if(c == 'C'){
+            cnt[1]--;
+        }
+        if(c == 'G'){
+            cnt[2]--;
+        }
+        if(c == 'T'){
+            cnt[3]--;
+        }
+    }
 
+    public static void check(){
+        for(int i =0;i<4;i++){
+            if(cnt[i]<arr[i]) return;
+        }
+        res++;
+    }
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        res = 0;
+        arr = new int[4];
+        cnt = new int[4];
+
+        str = br.readLine();
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<4;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int l =0;
+        int r = M-1;
+
+        for(int i =0;i<M;i++){
+            char c = str.charAt(i);
+            add(c);
+        }
+        check();
+
+        while(r<N-1){
+            sub(str.charAt(l));
+            l++;
+            r++;
+            add(str.charAt(r));
+
+            check();
+        }
+
+        System.out.println(res);
+
+    }
 }
