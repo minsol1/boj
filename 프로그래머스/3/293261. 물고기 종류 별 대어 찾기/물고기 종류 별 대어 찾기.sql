a8f0aa1b70e377230902d4b127f284a6f680e009
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-06-30 09:53:37 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,10 +1,7 @@
 -- 코드를 작성해주세요
-select fi.id ID, fni.FISH_NAME ,fi.LENGTH
+select fi.ID, fni.FISH_NAME, fi.LENGTH
 from FISH_INFO fi left join FISH_NAME_INFO fni
 on fi.FISH_TYPE = fni.FISH_TYPE
-where (fi.FISH_TYPE,fi.LENGTH) in(select FISH_INFO.FISH_TYPE, max(LENGTH)
-                                  from FISH_INFO
-                                  where LENGTH is not null
-                                  group by FISH_INFO.FISH_TYPE
-                                  )
-order by fi.ID
+where (fi.FISH_TYPE, fi.LENGTH) in(select FISH_TYPE, max(LENGTH) LENGTH
+      from FISH_INFO 
+      group by FISH_TYPE)
