17bf32f3ba74adaf525bd8fa06149abfd11754cc
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-07-02 18:15:26 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,9 +1,8 @@
-select ID ,
-case 
-    when NTILE(4) over (order by SIZE_OF_COLONY) =1 then "LOW"
-    when NTILE(4) over (order by SIZE_OF_COLONY) =2 then "MEDIUM"
-    when NTILE(4) over (order by SIZE_OF_COLONY) =3 then "HIGH"
-    else "CRITICAL"
+select ID, case 
+when NTILE(4) over (order by SIZE_OF_COLONY desc) = 1 then 'CRITICAL'
+when NTILE(4) over (order by SIZE_OF_COLONY desc) = 2 then 'HIGH'
+when NTILE(4) over (order by SIZE_OF_COLONY desc) = 3 then 'MEDIUM'
+else 'LOW'
 end
 COLONY_NAME
 from ECOLI_DATA
