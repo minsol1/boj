b723d42f5a4f809e2bac7675d758434ff93fe874
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-07-02 12:20:17 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 260 ms, Memory: 13100 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,51 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb =new StringBuilder();
+    public static void main(String[]args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> q = new ArrayDeque<>();
+        sb.append('<');
 
-		int N = Integer.parseInt(st.nextToken()); 
-		int K = Integer.parseInt(st.nextToken()); 
-		ArrayList<Integer> res = new ArrayList<>();
-		
-		Queue<Integer> q =new LinkedList<Integer>();
-		for(int i=1;i<N+1;i++) {
-			q.add(i);
-		}
-		int c =1;
-		while(!q.isEmpty()) {
-			int n = q.poll();
-			if(c%K==0) {
-				res.add(n);
-			}else {
-				q.add(n);
-			}
-			c++;
-		}
-		sb.append('<');
-		for(int i=0;i<res.size();i++) {
-			if(i==0) {
-				sb.append(res.get(i));
-				
-			}else {
-				sb.append(", ").append(res.get(i));
-				
-			}
-		}
-		sb.append('>');
-		
-		System.out.println(sb);
-	}
-	
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int cnt =1;
 
+        for(int i =1;i<=N;i++){
+            q.add(i);
+        }
+
+        while(!q.isEmpty()){
+            if(cnt%K==0){
+                sb.append(q.poll());
+                if(q.size()>0) sb.append(", ");
+                else sb.append(">");
+            }
+            else{
+                q.add(q.poll());
+            }
+            cnt++;
+        }
+
+        System.out.println(sb);
+
+
+    }
 }
