68d20162300e72d1122ee6705144d8e8f7cfa145
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-07-02 12:46:09 +0900

    [Silver IV] Title: 카드2, Time: 112 ms, Memory: 23792 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,29 +1,27 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		
-		Queue<Integer> q = new LinkedList<Integer>();
-		for(int i=1;i<N+1;i++) {
-			q.add(i);
-		}
-		
-		while(q.size()!=1) {
-			q.poll();
-			q.add(q.poll());
-		}
-		
-		System.out.println(q.peek());
- 
-	}
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
+        Queue<Integer> q = new ArrayDeque<>();
+        for(int i =1;i<=N;i++){
+            q.add(i);
+        }
+        int cnt=1;
+        while(q.size()>1){
+            if(cnt%2==1){
+                q.poll();
+            }
+            else{
+                q.add(q.poll());
+            }
+            cnt++;
+        }
+
+        System.out.println(q.peek());
+    }
 }
