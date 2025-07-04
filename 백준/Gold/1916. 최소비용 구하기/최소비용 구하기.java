d9010cb883f246d2bf453318c0c64d7f403df1d7
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-07-04 10:59:40 +0900

    [Gold V] Title: 최소비용 구하기, Time: 352 ms, Memory: 50804 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,96 +1,79 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 class Node implements Comparable<Node>{
-	int v;
-	int w;
-	
-	public Node(int v, int w) {
-		this.v = v;
-		this.w = w;
-	}
-	
-	@Override
-	public int compareTo(Node o) {
-		// TODO Auto-generated method stub
-		return this.w -o.w;
-	}
+    int num;
+    int cost;
+
+    public Node(int num, int cost){
+        this.num = num;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return cost -o.cost;
+    }
 }
 
 public class Main {
-	static int[] dist;
-	static ArrayList[] li;
-	static boolean[] visited;
-	static int N;
-	static int M;
-	static int S;
-	static int E;
-	
-	static int di(int s) {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.add(new Node(s,0));
-		dist[s] = 0;
-		
-		while(!pq.isEmpty()) {
-			Node now = pq.poll();
-			if(!visited[now.v]) {
-				visited[now.v] = true;
-				
-				int len = li[now.v].size();
-				for(int i=0;i<len;i++) {
-					Node next = (Node) li[now.v].get(i);
-					if(!visited[next.v] && dist[next.v] > dist[now.v]+ next.w) {
-						dist[next.v] = dist[now.v]+ next.w;
-						pq.add(new Node(next.v, dist[next.v]));
-					}
-				}
-			}
-		}
-		
-		return dist[E];
-		
-	}
-	
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
-		
-		li = new ArrayList[N+1];
-		dist = new int[N+1];
-		visited = new boolean[N+1];
-		
-		for(int i=0; i<N+1; i++) {
-			li[i] = new ArrayList<Node>();
-			dist[i] = Integer.MAX_VALUE;
-		}
-		
-		for(int i=0;i<M;i++) {
-			st = new StringTokenizer(br.readLine());
-			
-			int v = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			
-			li[v].add(new Node(e,w));
-			
-		}
-		
-		st = new StringTokenizer(br.readLine());
-		S = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		
-		
-		System.out.println(di(S));
-		
-	}
+    public static int N,M,S,E;
+    public static ArrayList[] arr;
+    public static int[] cost;
+
+    public static void bfs(){
+        PriorityQueue<Node> q = new PriorityQueue<>();
+        q.add(new Node(S,0));
+        cost[S] = 0;
+
+        while(!q.isEmpty()){
+            Node now = q.poll();
+            if(now.num==E) return;
+
+            for(int i = 0; i<arr[now.num].size(); i++){
+                Node next = (Node)arr[now.num].get(i);
+                int nc = now.cost + next.cost;
+
+                if(cost[next.num] >nc){
+                    cost[next.num] = nc;
+                    q.add(new Node(next.num,nc));
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        cost = new int[N+1];
+        arr = new ArrayList[N+1];
+        Arrays.fill(cost, Integer.MAX_VALUE);
+        StringTokenizer st;
+
+        for(int i =0;i<N+1;i++){
+            arr[i] = new ArrayList<Node>();
+        }
+
+        for(int i =0;i<M;i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            arr[a].add(new Node(b,c));
+
+        }
+        st = new StringTokenizer(br.readLine());
+
+        S = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        bfs();
+
+        System.out.println(cost[E]);
+
+    }
 }
