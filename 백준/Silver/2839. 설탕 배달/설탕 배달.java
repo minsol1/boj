d3c463ad8ea2d9e79d646d6140ee276c595208ee
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-07-09 12:08:24 +0900

    [Silver IV] Title: 설탕 배달, Time: 68 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,35 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] dp = new int[5001];
-		
-		Arrays.fill(dp, -1);
-		dp[3]=1;
-		dp[5]=1;
-		
-		for(int i=6;i<N+1;i++) {
-			if ((dp[i-3]!=-1) && (dp[i-5]!=-1)){
-				dp[i]= (dp[i-3]<dp[i-5])?dp[i-3]+1 :dp[i-5]+1; 
-			}
-			else if (dp[i-5] !=-1) {
-				dp[i]=dp[i-5]+1;
-			}
-			else if (dp[i-3] !=-1) {
-				dp[i]=dp[i-3]+1;
-			}
-		}
-		
-		System.out.println(dp[N]);
-		
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-	}
+        int[] dp = new int[N+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[3] = 1;
+        if(N>=5) dp[5] =1;
 
+        for(int i =4;i<=N;i++){
+            if(dp[i-3]!=Integer.MAX_VALUE) dp[i]= dp[i-3]+1;
+            if(i>5 && dp[i-5]!=Integer.MAX_VALUE){
+                dp[i]= Math.min(dp[i],dp[i-5]+1);
+            }
+        }
+
+        if(dp[N] == Integer.MAX_VALUE) System.out.println(-1);
+        else System.out.println(dp[N]);
+
+    }
 }
