daea4b0a9af02470d05132ab5770ac29f976a38c
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-07-10 12:06:34 +0900

    [Silver II] Title: 색종이 만들기, Time: 108 ms, Memory: 13172 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,55 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	static int N;
-	static int wcnt;
-	static int bcnt;
-	static int[][] arr;
-	
-	static void cnt(int x,int y,int n) {
-		int cnt=0;
-		for(int i=x; i<x+n;i++) {
-			for(int j=y;j<y+n;j++) {
-				cnt+=arr[i][j];
-			}
-		}
-		if(cnt==0) {
-			wcnt++;
-		}
-		else if(cnt== n*n) {
-			bcnt++;
-		}
-		else {
-			cnt(x, y, n/2);
-			cnt(x, y+n/2, n/2);
-			cnt(x+n/2, y, n/2);
-			cnt(x+n/2, y+n/2, n/2);
-		}
-	}
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		N = Integer.parseInt(br.readLine());
-		arr = new int[N][N];
-		wcnt =0;
-		bcnt=0;
-		
-		for(int i=0;i<N;i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0;j<N;j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		cnt(0,0,N);
-		System.out.println(wcnt);
-		System.out.println(bcnt);
-		
-		
-	}
+    public static int N,blue, white;
+    public static int[][] arr;
+
+    public static void dfs(int x,int y, int n ){
+        int p = arr[x][y];
+
+        for(int i=0;i<n;i++){
+            for(int j =0;j<n;j++){
+                if(arr[x+i][y+j]!=p){
+                    dfs(x,y,n/2);
+                    dfs(x+n/2,y,n/2);
+                    dfs(x,y+n/2,n/2);
+                    dfs(x+n/2,y+n/2,n/2);
+                    return;
+                }
+            }
+        }
+        if(p==1){
+            blue++;
+        }
+        else{
+            white++;
+        }
+
+    }
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+        blue =0;
+        white = 0;
+
+
+        for(int i=0;i<N;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for(int j =0;j<N;j++){
+                arr[i][j]= Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dfs(0,0,N);
+
+        System.out.println(white);
+        System.out.println(blue);
+
+    }
 }
