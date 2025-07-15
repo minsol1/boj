670720ec9039a3474c1029bd7ceb178de14932c2
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-07-15 11:56:59 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 216 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -1,66 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N,res;
-	static int[][] arr;
-	
-	static void dfs(int idx, int cnt) {
-		if(idx == N) {
-			res = Math.max(res, cnt);
-			return;
-		}
-		if(arr[idx][0]<=0) {
-			dfs(idx+1 , cnt);
-		}
-		else {
-			int c=0;
-			for(int i=0;i<N;i++) {
-				if(arr[i][0]>0&& i!=idx) {
-					c++;
-				}
-			}
-			if(c==0) {
-				dfs(idx+1, cnt);
-			}
-			else {
-				for(int i =0;i<N;i++) {
-					if(arr[i][0]>0 && i!=idx) {
-						arr[idx][0] -= arr[i][1];
-						arr[i][0] -= arr[idx][1];
-						c=0;
-						c = arr[idx][0]<=0? c+1 :c;
-						c = arr[i][0]<=0? c+1 :c;
-						dfs(idx+1, cnt+c);
-						arr[idx][0] += arr[i][1];
-						arr[i][0] += arr[idx][1];
-						
-					}
-				}
-				
-			}
-		}
-		
-	}
+    public static int N,res;
+    public static int[][] arr;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N = Integer.parseInt(br.readLine());
-		arr = new int[N][2];
-		res = 0;
+    public static void dfs(int idx, int cnt){
+        if(idx >= N){
+            res = cnt>res ? cnt:res;
+            return;
+        }
 
-		for(int i =0;i<N;i++) {
-			st = new StringTokenizer(br.readLine());
-			arr[i][0] = Integer.parseInt(st.nextToken());
-			arr[i][1] = Integer.parseInt(st.nextToken());
-		}
-		
-		dfs(0,0);
-		
-		System.out.println(res);
-	}
+        if(arr[idx][0]<=0) {
+            dfs(idx+1,cnt);
+            return;
+        }
 
+        for(int i =0;i<N;i++){
+            if(i == idx) continue;
+
+            if(arr[i][0]>0){
+                int c =0;
+                arr[i][0]-=arr[idx][1];
+                arr[idx][0]-=arr[i][1];
+
+                if(arr[i][0]<=0) c++;
+                if(arr[idx][0]<=0) c++;
+
+                dfs(idx+1, cnt+c);
+
+                arr[i][0]+=arr[idx][1];
+                arr[idx][0]+=arr[i][1];
+
+            }
+            else dfs(idx+1,cnt);
+        }
+
+    }
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        res = 0;
+        arr=new int[N][2];
+
+        for(int i =0;i<N;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(0,0);
+
+        System.out.println(res);
+
+    }
 }
