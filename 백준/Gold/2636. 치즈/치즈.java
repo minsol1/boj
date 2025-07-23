e0e38e81bfc46b49fe5810aa9403639ab975df19
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-07-23 13:00:19 +0900

    [Gold IV] Title: 치즈, Time: 80 ms, Memory: 12172 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,83 +1,69 @@
-//치즈 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N,M;
-	static int[][] arr;
-	static boolean[][] visited;
-	static int[] dx = {1,0,-1,0};
-	static int[] dy = {0,1,0,-1};
-	static int res,time;
-	static int cnt;
-	 
-	static void bfs() {
-		Queue<int[]> q  = new LinkedList<int[]>();
-		q.add(new int[] {0,0});
-		visited = new boolean[N][M];
-		visited[0][0] = true;
-		int c =0;
-		time++;
 
-		while(!q.isEmpty()) {
-			int[] now = q.poll();
-			
-			for(int d = 0;d<4 ;d++) {
-				int nx = now[0] + dx[d];
-				int ny = now[1] + dy[d];
-				if(nx<0 || nx>=N || ny<0 || ny>=M || visited[nx][ny]) {
-					continue;
-				}
-				if(arr[nx][ny]==0) { // 공기면 방문
-					q.add(new int[] {nx,ny});
-					visited[nx][ny] = true;
-				}else { // 치즈 녹임
-					visited[nx][ny] = true;
-					arr[nx][ny] =0;
-					c++;
-				}
-			}
-		}
-		
-		cnt -= c;
-		
-		if( cnt ==0) {
-			res = c;
-			return;
-		}
-		else {
-			bfs();
-		}
-	}
+    public static int N,M;
+    public static int[][] arr;
+    public static boolean[][] visited;
+    public static int[] dx = {0,1,0,-1};
+    public static int[] dy = {1,0,-1,0};
+    public static int time,res;
+    public static Queue<int[]>q;
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[N][M];
-		cnt =0;
-		time = 0;
-		
-		for(int i =0;i<N ; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j =0;j<M ;j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-				if(arr[i][j]==1) {
-					cnt++;
-				}
-			}
-		}
-		bfs();
-		System.out.println(time);
-		System.out.println(res);
-		
-	}
+    public static void bfs(){
+        Queue<int[]> q2 = new ArrayDeque<>();
 
+        while(!q.isEmpty()){
+            int[] now = q.poll();
+
+            for(int d = 0; d<4;d++){
+                int nx = now[0]+ dx[d];
+                int ny = now[1]+ dy[d];
+
+                if(nx <0 || nx>=N || ny<0 || ny>=M || visited[nx][ny]) continue;
+
+                if(arr[nx][ny]==0){
+                    q.add(new int[]{nx,ny});
+                }
+                else{
+                    q2.add(new int[]{nx,ny});
+                }
+                visited[nx][ny]= true;
+            }
+        }
+        q= q2;
+        if(!q2.isEmpty()){
+            res = q2.size();
+        }
+    }
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N][M];
+        visited = new boolean[N][M];
+        q = new ArrayDeque<>();
+        time = 0;
+        res = 0;
+
+        for(int i =0;i<N;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j =0;j<M;j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        q.add(new int[]{0,0});
+        visited[0][0]= true;
+        while(!q.isEmpty()){
+            time++;
+            bfs();
+        }
+        System.out.println(time-1);
+        System.out.println(res);
+
+    }
 }
