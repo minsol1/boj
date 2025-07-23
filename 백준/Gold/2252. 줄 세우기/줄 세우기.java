abcbd70cd6a4215ee73d7d5c7c64c582914dc733
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-07-23 11:17:10 +0900

    [Gold III] Title: 줄 세우기, Time: 600 ms, Memory: 46944 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] cnt = new int[N+1];
+        ArrayList[] arr = new ArrayList[N+1];
+
+        for(int i =0;i<N+1;i++){
+            arr[i] = new ArrayList<>();
+        }
+
+        for(int i =0;i<M;i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            cnt[b]++;
+            arr[a].add(b);
+        }
+
+        Queue<Integer> q = new ArrayDeque<>();
+
+        for(int i =1; i<N+1;i++){
+            if(cnt[i]==0) q.add(i);
+        }
+
+        while(!q.isEmpty()){
+            int now = q.poll();
+            System.out.print(now+" ");
+            for(int i =0;i<arr[now].size();i++){
+                int n = (int)arr[now].get(i);
+                cnt[n]--;
+                if(cnt[n]==0) q.add(n);
+            }
+        }
+
+    }
+}
