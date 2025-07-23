93ed3a2ad197c707882e0a78655459a80689907d
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-07-23 11:47:34 +0900

    [Gold V] Title: 적록색약, Time: 76 ms, Memory: 12644 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,83 +1,76 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static class Pos{
-		int row; int col;
 
-		public Pos(int row, int col) {
-			super();
-			this.row = row;
-			this.col = col;
-		}
-	}
-	static int retColorBlindness=0;
-	static int retNormal=0;
-	static char MAPBlindness[][];
-	static char MAP[][];
-	static int GO[][] = {{-1, 0},{0, -1}, {1, 0}, {0,1}};
-	static boolean [][] visited;
-	static BufferedReader br;
-	static int N;
-	static int BFS(int curRow, int curCol, char curChar, boolean flag) {	//flag true일 땐 정상, flag false 일 땐 색맹
-		Queue<Pos> q = new ArrayDeque<>();
-		q.add(new Pos(curRow, curCol));
-		while(!q.isEmpty()) {
-			int cr = q.peek().row;
-			int cc = q.peek().col;
-			q.poll();
-			for(int i=0; i<4; i++) {
-				int nextRow = cr +GO[i][0];
-				int nextCol = cc + GO[i][1];
-				if(nextRow <0 || nextCol <0 || nextRow >=N || nextCol >=N) continue;
-				if(visited[nextRow][nextCol]) continue;
-				if(flag && MAP[nextRow][nextCol] != curChar) continue;
-				if(!flag && MAPBlindness[nextRow][nextCol] != curChar) continue;
-				visited[nextRow][nextCol] = true;
-				q.add(new Pos(nextRow, nextCol));
-			}
-		}
-		
-		
-		return 1;
-	}
-	
-	public static void main(String[] args) throws IOException{
-		br= new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		MAP = new char[N][N];
-		MAPBlindness = new char[N][N];
-		for(int i=0; i<N; i++) {
-			char[] str = br.readLine().toCharArray();
-			MAP[i] = str;
-			for(int j =0; j< str.length; j++) {
-				if(MAP[i][j] == 'G')
-					MAPBlindness[i][j] = 'R';
-				else MAPBlindness[i][j] = MAP[i][j];
-			}
-		}
-		visited = new boolean[N][N];
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				if(!visited[i][j]) {
-					retNormal+= BFS(i,j,MAP[i][j], true);
-				}
-			}
-		}
-		visited = new boolean[N][N];
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				if(!visited[i][j]) {
-					retColorBlindness+= BFS(i,j,MAPBlindness[i][j], false);
-				}
-			}
-		}
-		
-		System.out.println(retNormal + " " +retColorBlindness);
-		
-	}
-		
+    public static int N;
+    public static char[][] arr;
+    public static boolean[][] visited;
+    public static int[] dx = {0,1,0,-1};
+    public static int[] dy = {1,0,-1,0};
+    public static int res1, res2;
+
+    public static void bfs(int x, int y, char color, boolean rg){
+        Queue<int[]> q = new ArrayDeque<>();
+        visited[x][y] = true;
+        q.add(new int[]{x,y});
+
+        while(!q.isEmpty()){
+            int[] now = q.poll();
+
+            for(int d = 0; d<4;d++){
+                int nx = now[0]+dx[d];
+                int ny = now[1]+dy[d];
+
+                if(nx<0 || nx>=N || ny<0 || ny>=N || visited[nx][ny]) continue;
+                if((color == 'R' || color == 'G') && rg ){
+                    if(arr[nx][ny] == 'R' || arr[nx][ny] == 'G'){
+                        q.add(new int[] {nx,ny});
+                        visited[nx][ny] = true;
+                    }
+                }
+                else{
+                    if(arr[nx][ny] == color){
+                        q.add(new int[] {nx,ny});
+                        visited[nx][ny] = true;
+                    }
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new char[N][N];
+
+        for(int i =0;i<N;i++){
+            String st = br.readLine();
+            for(int j=0;j<N;j++){
+                arr[i][j] = st.charAt(j);
+            }
+        }
+
+        visited = new boolean[N][N];
+        for(int i =0;i<N;i++){
+            for(int j =0;j<N;j++){
+                if(!visited[i][j]){
+                    res1++;
+                    bfs(i,j,arr[i][j],false);
+                }
+            }
+        }
+        visited = new boolean[N][N];
+        for(int i =0;i<N;i++){
+            for(int j =0;j<N;j++){
+                if(!visited[i][j]){
+                    res2++;
+                    bfs(i,j,arr[i][j],true);
+                }
+            }
+        }
+        System.out.println(res1+" "+res2);
+
+    }
 }
