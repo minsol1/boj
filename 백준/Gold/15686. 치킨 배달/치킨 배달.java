e2d9937078462b3fd56a47720a3229239a513107
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-07-30 17:25:42 +0900

    [Gold V] Title: 치킨 배달, Time: 440 ms, Memory: 137452 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,105 +1,97 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N,M;
-	static int[][] arr;
-	static int[][] visited;
-	static int[] dx = {1,0,-1,0};
-	static int[] dy = {0,1,0,-1};
-	static Deque<int[]> q; 
-	static ArrayList<int[]> pos;
-	static ArrayList<int[]> clist;
-	static int res;
-	
-	static int bfs() {
-		int c = 0;
-		while(!q.isEmpty()) {
-			int[] now = q.poll();
-			for(int d=0;d<4;d++) {
-				int nx = now[0] + dx[d];
-				int ny = now[1] + dy[d];
-				if(nx<0 || nx>=N || ny<0 || ny>=N) {
-					continue;
-				}
-				if(visited[nx][ny]==-1) {
-					visited[nx][ny] = visited[now[0]][now[1]]+1;
-					q.add(new int[] {nx,ny});
-					
-					if(arr[nx][ny]==1) {
-						c+= visited[nx][ny];
-					}
-				}
-			}
-		}
-		
-		return c;
-	}
-	
-	static void bt(int idx, int cnt) {
-		if(cnt == M) { // 치킨집 m개 조합 찾으면 bfs 
-			
-			for(int i=0;i<N;i++) {
-				Arrays.fill(visited[i], -1);
-			}
-			
-			for(int i = 0;i<M ;i++) {
-				q.add(clist.get(i));
-				visited[clist.get(i)[0]][clist.get(i)[1]]++;
-			}
-			
-			res = Math.min(res, bfs() ); // 거리합이 최소보다 작으면 리셋
-			return;
-		}
-		
-		for(int i= idx ; i<pos.size();i++) {
-			clist.add(cnt, pos.get(i));
-			bt(i+1, cnt+1);
-		}
-		
-	}
+    public static int N, M ,res, c_cnt;
+    public static int[][] arr;
+    public static int[][] visited;
+    public static int[] dx = {0,1,0,-1};
+    public static int[] dy = {1,0,-1,0};
+    public static ArrayList<int[]> chicken;
+    public static ArrayList<int[]> select;
+    public static Queue<int []> q;
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		arr = new int[N][N]; // 맵 
-		visited = new int[N][N]; // bfs 할때 
-		pos = new ArrayList<>(); // 치킨집 위치 
-		clist = new ArrayList<>(); // 선택된 치킨집  
-		q = new ArrayDeque<int[]>(); // 방문큐
-		res = 10000000; // 결과 
-		
-		for(int i=0 ; i<N ; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0;j<N;j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-				if( arr[i][j]==2) { // 치킨집 찾으면 위치 넣어줌 
-					pos.add(new int[] {i,j});
-				}
-			}
-		}
-		
-		bt(0,0);
-//		for(int i=0;i<N;i++) {
-//			for(int j=0;j<N;j++) {
-//				System.out.print(visited[i][j]+" ");
-//			}
-//			System.out.println(" ");
-//		}
-		System.out.println(res);
+    public static void dfs(int idx, int cnt){
+        if(cnt == M)
+        {
+            visited = new int[N][N];
 
-	}
+            for(int i =0;i<N;i++){
+                for(int j =0;j<N;j++)
+                {
+                    visited[i][j] = -1;
+                }
+            }
+            for(int i =0; i< select.size(); i ++){
+                int[] now = select.get(i);
+                visited[now[0]][now[1]] ++;
+                q.add(new int[] {now[0],now[1]});
+            }
+            bfs();
+            return;
+        }
+        if(idx >=c_cnt)
+            return;
 
+        select.add(chicken.get(idx));
+        dfs(idx + 1, cnt + 1);
+        select.remove(select.size() - 1);
+        dfs(idx + 1, cnt);
+    }
+
+    public static void bfs(){
+        int sum = 0;
+        while(!q.isEmpty())
+        {
+            int[] now = q.poll();
+
+            for(int d = 0; d<4;d++)
+            {
+                int nx = now[0] + dx[d];
+                int ny = now[1] + dy[d];
+
+                if(nx < 0 || nx>=N || ny <0 || ny >=N) continue;
+                if(visited[nx][ny]==-1){
+                    if(arr[nx][ny] == 1){
+                        sum += visited[now[0]][now[1]]+1;
+                    }
+                    visited[nx][ny] = visited[now[0]][now[1]]+1;
+                    q.add(new int[]{nx,ny});
+                }
+            }
+        }
+        res = res < sum ?res : sum;
+    }
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        c_cnt = 0;
+        res = Integer.MAX_VALUE;
+        arr = new int[N][N];
+        chicken = new ArrayList<>();
+        select = new ArrayList<>();
+        q = new ArrayDeque<>();
+
+        for(int i =0;i<N;i++)
+        {
+            st = new StringTokenizer(br.readLine());
+            for(int j =0;j<N;j++)
+            {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if(arr[i][j] == 2){
+                    chicken.add(new int[]{i,j});
+                    c_cnt++;
+                }
+            }
+        }
+
+        dfs(0,0);
+
+        System.out.println(res);
+
+    }
 }
