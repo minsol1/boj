9d4ff4df0d867be197f88932a7f2fe3a1d7d00e8
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-08-13 10:45:06 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 536 ms, Memory: 92828 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,84 +1,79 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-	
-	static int N;
-	static int M;
-	static int[][] arr;
-	static int[] dx = {1,0,-1,0};
-	static int[] dy = {0,1,0,-1};
-	static int[][][] visited;
-	
-	public static void bfs(int i, int j) {
-		Queue< int[]> q = new ArrayDeque<int[]>();
-		visited[0][i][j]++;
-		q.add(new int[] {0,i,j});
-		
-		while(!q.isEmpty()) {
-			int[] now = q.poll();
-			
-			if(now[1]==N-1 && now[2] == M-1) {
-				int res =visited[0][now[1]][now[2]]==0?visited[1][now[1]][now[2]]:visited[0][now[1]][now[2]];
-				System.out.println(res);
-				return;
-			}
-			for(int d =0;d<4 ;d++) {
-				int nx = now[1] + dx[d];
-				int ny = now[2] + dy[d];
-				
-				if (nx<0|| nx>=N || ny<0 || ny>=M ) {
-					continue;
-				}
-				
-				if(arr[nx][ny]==0) {
-					if(now[0]==0 && visited[0][nx][ny]==0) {
-						visited[0][nx][ny] = visited[0][now[1]][now[2]]+1;
-						q.add(new int[] {0,nx,ny});
-					}
-					else if(now[0]==1&& visited[1][nx][ny]==0) {
-						visited[1][nx][ny] = visited[1][now[1]][now[2]]+1;
-						q.add(new int[] {1,nx,ny});
-						
-					}
-				}
-				else {
-					if(now[0]==0) {
-						visited[1][nx][ny] = visited[0][now[1]][now[2]]+1;
-						q.add(new int[] {1,nx,ny});
-						
-					}
-				}
-			}
-		}
-		System.out.println(-1);
-	}
+class Node{
+    int x;
+    int y;
+    int w;
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		arr = new int[N][M];
-		visited = new int[2][N][M]; // 방문 체크, 벽부수고 안부순거리따로 관리 
-		
-		for(int i=0;i<N;i++) {
-			String str = br.readLine();
-			for(int j=0;j<M ; j++) {
-				arr[i][j] = str.charAt(j) -'0';
-			}
-		}
-		bfs(0,0);
-
-
-	}
-
+    public Node(int x, int y, int w){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+    }
 }
 
+public class Main {
+
+    public static int N,M;
+    public static int[][] arr;
+    public static int[][][] visited;
+    public static int[] dx = {0,1,0,-1};
+    public static int[] dy = {1,0,-1,0};
+
+    public static void bfs(){
+        Queue<Node> q = new ArrayDeque<>();
+        visited[0][0][0] = 1;
+        q.add(new Node(0,0,0));
+
+        while(!q.isEmpty()){
+            Node now = q.poll();
+
+            for(int d = 0; d<4;d++){
+                int nx = now.x + dx[d];
+                int ny = now.y + dy[d];
+
+                if(nx < 0 || nx>=N || ny <0 || ny >=M)
+                    continue;
+
+                if(arr[nx][ny] == 1){
+                    if(visited[nx][ny][1] ==0 && now.w ==0 ){
+                        q.add(new Node(nx,ny,1));
+                        visited[nx][ny][1] = visited[now.x][now.y][0]+1;
+                    }
+                    continue;
+                }
+
+                if(visited[nx][ny][now.w] == 0){
+                    q.add(new Node(nx,ny,now.w));
+                    visited[nx][ny][now.w] = visited[now.x][now.y][now.w]+1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N][M];
+        visited = new int[N][M][2];
+
+        for(int i =0;i<N;i++){
+            String str = br.readLine();
+            for(int j =0;j<M;j++){
+                arr[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        bfs();
+
+        if(visited[N-1][M-1][0] == 0 &&  visited[N-1][M-1][1]==0)
+            System.out.println(-1);
+        else if (visited[N-1][M-1][0] == 0 ||  visited[N-1][M-1][1]==0)
+            System.out.println(Math.max(visited[N-1][M-1][0], visited[N-1][M-1][1]));
+        else
+            System.out.println(Math.min(visited[N-1][M-1][0], visited[N-1][M-1][1]));
+    }
+}
