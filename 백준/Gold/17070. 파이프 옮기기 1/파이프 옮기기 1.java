5b182a7cd26d9791eecf71769afddcc0bf459fef
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-08-13 10:01:05 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 68 ms, Memory: 11688 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,67 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N;
-	static int[][] arr;
-	static int[][] visited;
-	
-	static void bfs() {
-		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] {0,0,1});
-		visited[0][1] =1;
-		
-		while(!q.isEmpty()) {
-			int[] now = q.poll();
-			int p = now[0];
-			int x = now[1];
-			int y = now[2];
-			
-			if(y+1<N && x+1<N && arr[x+1][y+1]==0 && arr[x][y+1]==0&& arr[x+1][y]==0) { // 대각선으로  이동하기 
-				q.add(new int[] {1,x+1,y+1});
-				visited[x+1][y+1] ++;
-			}
-			if(p!=2 && y+1<N&& arr[x][y+1]==0) { // 가로로 이동하기 
-				q.add(new int[] {0,x,y+1});
-				visited[x][y+1] ++;
-			}
-			if(p!=0&& x+1<N&& arr[x+1][y]==0) { // 세로로 이동하기 
-				q.add(new int[] {2,x+1,y});
-				visited[x+1][y] ++;
-			}
-			
-		}
-		
-	}
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		N = Integer.parseInt(br.readLine());
-		arr = new int[N][N];
-		visited = new int[N][N];
-		
-		for(int i=0;i<N;i++) {
-			st= new StringTokenizer(br.readLine());
-			for(int j=0;j<N;j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][N];
+        int[][][] dp = new int[N][N][3];
 
-		if(arr[N-1][N-1]==1) {
-			System.out.println(0);
-		}
-		else {
-			bfs();
-			System.out.println(visited[N-1][N-1]);
-		}
-		
-	}
+        for(int i =0;i<N;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j =0;j<N;j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        dp[0][1][0] = 1;
 
+        for(int i =0;i<N;i++){
+            for(int j =0;j<N;j++){
+                if(i ==0 && j ==1)
+                    continue;
+                if(j>0 && arr[i][j] != 1)
+                    dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][2];
+                if(i>0 && arr[i][j] != 1)
+                    dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2];
+                if(i>0 && j > 0 && arr[i][j] != 1 && arr[i-1][j] != 1&& arr[i][j-1] != 1)
+                    dp[i][j][2] = dp[i-1][j-1][0]+dp[i-1][j-1][1]+dp[i-1][j-1][2];
+
+            }
+        }
+        
+        System.out.println(dp[N-1][N-1][0]+dp[N-1][N-1][1]+dp[N-1][N-1][2]);
+
+    }
 }
