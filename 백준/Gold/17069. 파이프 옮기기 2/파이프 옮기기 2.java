410a179b66d43ad940cf81711bb2ea92e3d0d6ef
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-08-13 10:04:29 +0900

    [Gold IV] Title: 파이프 옮기기 2, Time: 68 ms, Memory: 12024 KB -BaekjoonHub

--- 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
@@ -1,41 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
-		int[][] arr = new int[N+1][N+1];
-		long[][][] dp = new long[3][N+1][N+1];
-		
-		for(int i = 1 ; i<N+1;i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j =1;j<N+1;j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		dp[0][1][2] =1;
-		
-		for(int i =1;i<N+1;i++) {
-			for(int j =3;j<N+1;j++) {
-				if(arr[i][j]==0) {
-					dp[0][i][j] = dp[0][i][j-1] +dp[1][i][j-1];//가로
-					dp[2][i][j] = dp[2][i-1][j] + dp[1][i-1][j]; //세로 
-					
-					if(arr[i-1][j]!=1&&arr[i][j-1]!=1 ) { // 대각선 
-						
-						dp[1][i][j] = dp[0][i-1][j-1] +dp[1][i-1][j-1]+dp[2][i-1][j-1];
-					}
-					
-				}
-			}
-		}
-		System.out.print(dp[0][N][N]+dp[1][N][N]+dp[2][N][N]);
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][N];
+        long[][][] dp = new long[N][N][3];
 
-	}
+        for(int i =0;i<N;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j =0;j<N;j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        dp[0][1][0] = 1;
 
+        for(int i =0;i<N;i++){
+            for(int j =0;j<N;j++){
+                if(i ==0 && j ==1)
+                    continue;
+                if(j>0 && arr[i][j] != 1)
+                    dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][2];
+                if(i>0 && arr[i][j] != 1)
+                    dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2];
+                if(i>0 && j > 0 && arr[i][j] != 1 && arr[i-1][j] != 1&& arr[i][j-1] != 1)
+                    dp[i][j][2] = dp[i-1][j-1][0]+dp[i-1][j-1][1]+dp[i-1][j-1][2];
+
+            }
+        }
+
+        System.out.println(dp[N-1][N-1][0]+dp[N-1][N-1][1]+dp[N-1][N-1][2]);
+
+    }
 }
