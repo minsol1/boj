e3500cd4bc9696e46ab0b2df1f5118d7660e0361
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-08-14 11:30:18 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 488 ms, Memory: 64944 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,103 +1,85 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-// 말되원 
 public class Main {
-	static int N,M,K ;
-	static int[][] arr;
-	static int[] hdx = {-1,-2,-2,-1,1,2,2,1}; 
-	static int[] hdy = {-2,-1,1,2,2,1,-1,-2};
-	static int[] dx = {1,0,-1,0};
-	static int[] dy = {0,1,0,-1};
-	static boolean[][][] visited;
-	
-	static class Node{
-		int cnt;
-		int x;
-		int y;
-		int len;
-		
-		public Node(int cnt, int x, int y, int len) {
-			super();
-			this.cnt = cnt;
-			this.x = x;
-			this.y = y;
-			this.len = len;
-		}
-		
-	}
-	
-	static void bfs() {
-		Queue<Node> q = new ArrayDeque<>();
-		visited[0][0][0] = true;
-		q.add(new Node(0,0,0,0));
-		
-		while(!q.isEmpty()) {
-			Node now = q.poll();
-			
-			if( now.x == N-1 && now.y == M-1) {
-				System.out.println(now.len);
-				return;
-			}
-			
-			if(now.cnt<K) {
-				for(int d = 0; d<8;d++) {
-					int nx = now.x + hdx[d];
-					int ny = now.y + hdy[d];
-					
-					if(nx<0 || nx>=N || ny < 0 || ny>=M) {
-						continue;
-					}
-					if(!visited[now.cnt+1][nx][ny] && arr[nx][ny]==0) {
-						visited[now.cnt+1][nx][ny] = true;
-						q.add(new Node(now.cnt+1,nx,ny,now.len+1));
-					}
-					
-				}
-			}
-			for(int d=0;d<4;d++) {
-				int nx = now.x + dx[d];
-				int ny = now.y + dy[d];
-				
-				if(nx<0 || nx>=N || ny < 0 || ny>=M) {
-					continue;
-				}
-				if(!visited[now.cnt][nx][ny] && arr[nx][ny]==0) {
-					visited[now.cnt][nx][ny] = true;
-					q.add(new Node(now.cnt,nx,ny,now.len+1));
-				}
-				
-			}
-		}
-		System.out.println(-1);
-		
-	}
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		K = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		M = Integer.parseInt(st.nextToken());
-		N = Integer.parseInt(st.nextToken());
-		
-		arr = new int[N][M];
-		visited = new boolean[K+1][N][M];
-		
-		for(int i =0;i<N;i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0;j<M;j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		bfs();
-		
+    public static int K,N,M;
+    public static int[][] arr;
+    public static int[][][] visited;
+    public static int[] dx = {0,1,0,-1};
+    public static int[] dy = {1,0,-1,0};
+    public static int[] hx = {1,2,2,1,-1,-2,-2,-1};
+    public static int[] hy = {-2,-1,1,2,-2,-1,1,2};
 
-	}
+    public static void bfs(){
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{0,0,0});
+        visited[0][0][0] = 0;
 
+        while(!q.isEmpty()){
+            int[] now = q.poll();
+
+            for(int d = 0; d<4; d++){
+                int nx = now[0] + dx[d];
+                int ny = now[1] + dy[d];
+
+                if(nx < 0 || nx >=N || ny<0 || ny >=M|| visited[nx][ny][now[2]] !=-1 || arr[nx][ny] !=0) continue;
+
+                q.add(new int[]{nx,ny,now[2]});
+                visited[nx][ny][now[2]] = visited[now[0]][now[1]][now[2]] +1;
+            }
+            if(now[2]<K){
+                for(int d = 0; d<8;d++){
+                    int nx = now[0] + hx[d];
+                    int ny = now[1] + hy[d];
+
+                    if(nx < 0 || nx >=N || ny<0 || ny >=M|| visited[nx][ny][now[2]+1] !=-1|| arr[nx][ny] !=0) continue;
+
+                    q.add(new int[]{nx,ny,now[2]+1});
+                    visited[nx][ny][now[2]+1] = visited[now[0]][now[1]][now[2]] +1;
+
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        K = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        arr = new int[N][M];
+        visited = new int[N][M][K+1];
+
+        for(int i =0;i<N;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j<M;j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i =0;i<N;i++){
+            for(int j =0;j<M;j++){
+                for(int k = 0; k<=K;k++){
+                    visited[i][j][k] = -1;
+                }
+            }
+        }
+
+        bfs();
+
+        int res = Integer.MAX_VALUE;
+
+        for(int k = 0; k<=K;k++){
+            int n = visited[N-1][M-1][k];
+            if(n == -1) continue;
+            res = res < n ?res : n;
+        }
+        if(res == Integer.MAX_VALUE ) res = -1;
+        System.out.println(res);
+
+    }
 }
