65f285410db2d72e138b922b334cb3031352fc97
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-08-14 12:10:44 +0900

    [Gold IV] Title: 연구소, Time: 408 ms, Memory: 105364 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,119 +1,86 @@
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
-// 연구소 
 public class Main {
-	static int N,M;
-	static int[][] arr;
-	static boolean[][] visited;
-	static int[] dx = {1,0,-1,0};
-	static int[] dy = {0,1,0,-1};
-	static int res=0;
-	static int cnt2,cnt1,cnt;
-	static Queue<int[]> q = new ArrayDeque<>();
-	static Queue<int[]> q2 = new ArrayDeque<>();
-	
-	static void dfs(int c) {
-		
-		if (c == 3) {
-			cnt=0; // 바이러스 수 
-			
-			for(int i =0;i<N;i++) {
-				for(int j =0;j<M;j++) {
-					if(arr[i][j] == 2) {
-						q.add(new int[] {i,j}); // 바이러스 큐에 넣음 
-						visited[i][j] = true;// 방문 
-						cnt++;
-					}
-				}
-			}
-			
-			bfs();
-			
-			while(!q2.isEmpty()) {
-				int[] now = q2.poll();
-				
-				visited[now[0]][now[1]] = false;
-			}
-			
-			return;
+    public static int N,M,res;
+    public static int[][] arr;
+    public static boolean[][] visited;
+    public static int[] dx = {0,1,0,-1};
+    public static int[] dy = {1,0,-1,0};
 
-		}
-		
-		for(int i =0;i<N;i++) {
-			for(int j =0;j<M;j++) {
-				if(arr[i][j] == 0) {
-					arr[i][j] = 1;
-					dfs(c+1);
-					arr[i][j] = 0;
-				}
-			}
-		}
-	}
-	
-	static void bfs() {
-		
-		while(!q.isEmpty()) {
-			int[] now = q.poll();
-//			if(cnt>cnt2) {
-//				return;
-//			}
-			
-			for(int d = 0; d<4;d++) {
-				int nx = now[0] + dx[d];
-				int ny = now[1] + dy[d];
-				
-				if(nx < 0 || nx >=N || ny <0 || ny>=M || visited[nx][ny]) {
-					continue;
-				}
-				if(arr[nx][ny] == 0) {
-					q.add(new int[] {nx,ny});
-					q2.add(new int[] {nx,ny});
-					visited[nx][ny] = true;
-					cnt++;
-				}
-				
-			}
-		}
-		
-		cnt2 = cnt;
-		res = res > N*M-cnt2-cnt1 ? res :N*M-cnt2-cnt1;
-		
-	}
+    public static void dfs(int x, int y, int cnt){
+        if( cnt == 3){
+            bfs();
+            int c=0;
+            for(int i =0; i<N;i++){
+                for(int j =0;j<M;j++){
+                    if(arr[i][j]== 0 && !visited[i][j]){
+                        c++;
+                    }
+                }
+            }
+            res = res > c ? res : c;
+            return;
+        }
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st =new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		arr = new int[N][M];
-		visited = new boolean[N][M];
-		cnt2 = N*M; // 바이러스 수 
-		cnt1 =0; // 벽 수 
-		
-		for(int i = 0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j =0; j<M ; j ++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-				
-				if(arr[i][j]==1) {
-					cnt1++;
-				}
-			}
-		}
-		cnt1 +=3;
-		
-		dfs(0);
-		System.out.println(res);
+        for(int i =x; i<N;i++){
+            for(int j =y; j<M;j++){
+                if( arr[i][j] == 0){
+                    arr[i][j] = 1;
+                    dfs(i, 0,cnt+1);
+                    arr[i][j] = 0;
+                }
+            }
+        }
+    }
 
-	}
+    public static void bfs(){
+        visited = new boolean[N][M];
+        Queue<int[]> q = new ArrayDeque<>();
+        for(int i =0;i<N;i++){
+            for(int j =0;j<M;j++){
+                if(arr[i][j] == 2){
+                    q.add(new int[]{i,j});
+                }
+            }
+        }
 
+        while(!q.isEmpty()){
+            int[] now = q.poll();
+
+            for(int d = 0; d<4;d++){
+                int nx = now[0] + dx[d];
+                int ny = now[1] + dy[d];
+
+                if(nx<0 || nx>=N || ny <0 || ny >=M || visited[nx][ny]) continue;
+
+                if(arr[nx][ny] == 0){
+                    q.add(new int[] {nx,ny});
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+
+    }
+
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        res = 0;
+        arr = new int[N][M];
+        for(int i = 0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j =0; j<M;j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dfs(0,0,0);
+
+        System.out.println(res);
+
+    }
 }
