85668a9dc5ecb42f17711270ae2127ed0db372aa
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-09-02 09:39:37 +0900

    [Gold V] Title: 평범한 배낭, Time: 136 ms, Memory: 13416 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,34 +1,24 @@
-// 배낭 챙기기 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] dp = new int[K+1];
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		int[] dp = new int[K+1];
-		
-		for(int i=0;i<N;i++) {
-			st = new StringTokenizer(br.readLine());
-			int w = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			
-			for(int j=K ;j>=w ; j--) {
-				if(dp[j]<dp[j-w]+v ) {
-					dp[j] = dp[j-w]+v;
-				}
-			}
-			
-		}
-		System.out.println(dp[K]);
-		
-	}
+        for(int i =0; i<N;i++){
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
 
+            for(int j = K; j>=w ;j-- ){
+                dp[j] = dp[j] > dp[j-w]+v ? dp[j] : dp[j-w]+v;
+            }
+        }
+
+        System.out.println(dp[K]);
+    }
 }
