e03b46311fa6c1801bba4fe097103f48aa122b17
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-09-11 12:35:38 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 96 ms, Memory: 14672 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -1,102 +1,91 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int x;
+    int y;
+    int k;
+
+    public Node(int x, int y, int k){
+        this.x =x;
+        this.y =y;
+        this.k =k;
+    }
+}
 
 public class Main {
-	static char[][] arr;
-	static boolean[][][] visited;
-	static int[] dx = {1,0,-1,0};
-	static int[] dy = {0,1,0,-1};
-	static int N,M;
-	static int res= Integer.MAX_VALUE;
-	static Queue<Node> q = new ArrayDeque<>();
-	
-	static class Node{
-		int x;
-		int y;
-		int cnt;
-		int key;
-		public Node(int x, int y, int cnt, int key) {
-			super();
-			this.x = x;
-			this.y = y;
-			this.cnt = cnt;
-			this.key = key;
-		}
-		
-	}
-	
-	static void bfs() {
-		
-		while(!q.isEmpty()) {
-			Node now = q.poll();
-			for(int d=0;d<4;d++) {
-				int nx = now.x + dx[d];
-				int ny = now.y + dy[d];
-				
-				if(nx<0|| nx>=N || ny<0 || ny>=M || arr[nx][ny]=='#'||visited[nx][ny][now.key]) {
-					continue;
-				}
-				if(arr[nx][ny]=='1') {
-					res = res<now.cnt+1? res : now.cnt+1;
-					return;
-				}
-				if(arr[nx][ny]=='.'||arr[nx][ny]=='0') {
-					visited[nx][ny][now.key]= true;
-					q.add(new Node(nx,ny,now.cnt+1,now.key));
-					
-				}else if(arr[nx][ny]>='A'&&arr[nx][ny]<='F' ){// 문 
-					if(((1<<(arr[nx][ny]-'A'))&now.key)>=1) {//키잇음 
-						visited[nx][ny][now.key]= true;
-						q.add(new Node(nx,ny,now.cnt+1,now.key));
-						
-					}
-				}else { // 키 
-					visited[nx][ny][now.key] = true;
-					visited[nx][ny][now.key|(1<<(arr[nx][ny]-'a'))]=true;
-					q.add(new Node(nx,ny,now.cnt+1, now.key|(1<<(arr[nx][ny]-'a')))); 
-					
-				}
-				
-				
-			}
-		}
-		
-		
-	}
+    public static char[][] arr;
+    public static int N,M,res;
+    public static int[] dx = {0,1,0,-1};
+    public static int[] dy = {1,0,-1,0};
+    public static int[][][] visited;
+    public static Queue<Node> q;
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		visited = new boolean[N][M][1<<6];
-		arr = new char[N][M];
-		
-		for(int i = 0; i<N;i++) {
-			String str = br.readLine();
-			for(int j =0;j<M;j++) {
-				arr[i][j] = str.charAt(j);
-				if(arr[i][j]=='0') {
-					q.add(new Node(i,j,0, 0));
-					visited[i][j][0] = true;
-				}
-			}
-		}
-		
-		bfs();
-		if(res == Integer.MAX_VALUE) {
-			System.out.println(-1);
-		}else {
-			
-			System.out.println(res);
-		}
-		
-	}
 
+    public static void bfs(){
+
+        while(!q.isEmpty()){
+            Node now = q.poll();
+            if(arr[now.x][now.y] == '1'){
+                res = visited[now.x][now.y][now.k];
+                return;
+            }
+
+            for(int d = 0; d<4;d++){
+                int nx = now.x + dx[d];
+                int ny = now.y + dy[d];
+
+                if(nx < 0 || nx >=N || ny < 0 || ny >=M || visited[nx][ny][now.k]!=-1 || arr[nx][ny] == '#') continue;
+                if(arr[nx][ny] >= 'A'  && arr[nx][ny]<= 'F'){
+                    int c = 1<<(arr[nx][ny] - 'A' +1);
+                    if( (c & now.k )== 0) continue;
+                }
+                int k = now.k;
+                if(arr[nx][ny] >= 'a'  && arr[nx][ny]<= 'f'){
+                    int c =1<< (arr[nx][ny] - 'a'+1 );
+                    k = k | c;
+                }
+
+                q.add(new Node(nx,ny,k));
+                visited[nx][ny][k]= visited[now.x][now.y][now.k] +1;
+
+            }
+        }
+
+    }
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        q = new ArrayDeque<>();
+        res = Integer.MAX_VALUE;
+
+        arr = new char[N][M];
+        visited = new int[N][M][1<<6+1];
+
+        for(int i =0;i<N;i++){
+            for(int j =0; j<M;j++){
+                Arrays.fill(visited[i][j], -1);
+            }
+        }
+
+        for(int i =0; i<N;i++){
+            String str = br.readLine();
+
+            for(int j =0; j<M;j++){
+                arr[i][j] = str.charAt(j);
+                if(arr[i][j]=='0') {
+                    q.add(new Node(i,j,0));
+                    visited[i][j][0] = 0;
+                }
+            }
+        }
+
+        bfs();
+
+        if(res == Integer.MAX_VALUE) System.out.println(-1);
+        else System.out.println(res);
+    }
 }
