de0426e2f99b36fe00dd2e153c2f181230fe410a
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-09-12 16:11:38 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 92 ms, Memory: 13048 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -3,44 +3,53 @@ import java.util.*;
 
 public class Main {
 
+    public static int N;
+    public static boolean f;
+    public static int[][] pos;
+    public static boolean[] visited;
+
+    public static void bfs(){
+        visited[0] = true;
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(0);
+
+        while(!q.isEmpty()){
+            int n = q.poll();
+            if(n == N+1){
+                f = true;
+                return;
+            }
+
+            for(int nx = 0; nx < N+2; nx++){
+                int d = Math.abs(pos[n][0] - pos[nx][0])+Math.abs(pos[n][1] - pos[nx][1]);
+
+                if(d <= 1000 && !visited[nx]){
+                    visited[nx] = true;
+                    q.add(nx);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
         for(int t = 0; t<T;t++){
-            int N = Integer.parseInt(br.readLine());
-            int[][] dist = new int[N+2][N+2];
-            int[][] pos = new int[N+2][2];
+            N = Integer.parseInt(br.readLine());
+            pos = new int[N+2][N+2];
+            visited = new boolean[N+2];
+            f = false;
 
             for(int i =0; i<N+2;i++){
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-
-                pos[i][0]= a;
-                pos[i][1]= b;
+                pos[i][0] = Integer.parseInt(st.nextToken());
+                pos[i][1] = Integer.parseInt(st.nextToken());
             }
+            bfs();
 
-            for(int i = 0; i< N+2; i++){
-                for(int j = 0;j<N+2; j++){
-                    int d = Math.abs(pos[i][0] - pos[j][0])+Math.abs(pos[i][1]- pos[j][1]);
-                    dist[i][j]= d;
-                }
-            }
-
-            for(int i =0; i<N+2; i ++){
-                for(int j =0; j< N+2 ; j++){
-                    for(int k =0; k<N+2; k++){
-                        int d = Math.min(dist[j][k], dist[j][i] + dist[i][k]);
-
-                        if(d <= 1000) dist[j][k] = 0;
-
-                    }
-                }
-            }
-            
-            if(dist[0][N+1] >1000) System.out.println("sad");
-            else System.out.println("happy");
+            if(f) System.out.println("happy");
+            else System.out.println("sad");
         }
     }
 }
