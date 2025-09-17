9d433ba6bce86733f0390cda5c70273e3f82365e
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-09-17 09:48:09 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,5 +1,9 @@
--- 코드를 입력하세요
-SELECT BOARD_ID,WRITER_ID,TITLE,PRICE,if(STATUS="SALE", "판매중",if(STATUS="RESERVED","예약중","거래완료")) STATUS
+select BOARD_ID,WRITER_ID,TITLE,PRICE,
+case 
+when STATUS = 'SALE' then "판매중"
+when STATUS = 'RESERVED' then "예약중"
+else "거래완료"
+end STATUS
 from USED_GOODS_BOARD
-where CREATED_DATE ="2022-10-05"
+where CREATED_DATE = '2022-10-05'
 order by BOARD_ID desc
