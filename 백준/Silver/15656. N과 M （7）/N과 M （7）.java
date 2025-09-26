9f1f70e9c7cabd225eaa582fc3fb1a805e92133e
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-09-26 14:12:14 +0900

    [Silver III] Title: N과 M (7), Time: 544 ms, Memory: 220196 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int N,M;
+    public static int[] arr;
+    public static StringBuilder sb;
+
+    public static void dfs(int cnt, int[] res){
+        if(cnt == M){
+            for(int num : res){
+                sb.append(num).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for(int i =0; i<N ; i++){
+            res[cnt] = arr[i];
+            dfs(cnt+1, res);
+        }
+    }
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i =0; i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs(0,new int[M]);
+
+        System.out.println(sb);
+
+    }
+}
