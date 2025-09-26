2cd65da75c4a4b8ab9d7d1940e7787c3f1e1f037
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-09-26 18:33:56 +0900

    [Gold III] Title: 양팔저울, Time: 72 ms, Memory: 12852 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.java
+++ 백준/Gold/2629. 양팔저울/양팔저울.java
@@ -5,43 +5,44 @@ public class Main {
 
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //가지고 있는 추
-        int N = Integer.parseInt(br.readLine());
-        int[] chu = new int[N+1];
+        int N = Integer.parseInt(br.readLine()); // 추 개수
+        int[] arr = new int[N]; // 추
+        int sum=0; // 추 합
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i =1;i<N+1;i++){
-            chu[i] = Integer.parseInt(st.nextToken());
-        }
-
-        // 확인할 무게
-        int M = Integer.parseInt(br.readLine());
-        int[] arr = new int[M];
-        st = new StringTokenizer(br.readLine());
-        for(int i =0;i<M;i++){
+        for(int i =0; i<N;i++){
             arr[i] = Integer.parseInt(st.nextToken());
+            sum+=arr[i];
         }
 
-        boolean[][] dp = new boolean[N+1][40001];
+        boolean[][] dp = new boolean[N][sum*2+1]; // 만들수있는 경우
+        dp[0][arr[0]]  = true;
         dp[0][0] = true;
 
-        for(int i =1 ; i<N+1; i++){
-            for(int j =0;j<40001;j++){
-                if(j == chu[i]) dp[i][j] = true;
-
-                if(j+chu[i]<40001) dp[i][j] |= dp[i-1][j+chu[i]];
-
-                dp[i][j] |= dp[i-1][Math.abs(j-chu[i])];
-
+        for(int i =1; i<N;i++){
+            for(int j = 0 ; j<=sum ; j++){
                 if(dp[i-1][j]){
-                    dp[i][j]= true;
+                    dp[i][j] = true;
+                    dp[i][Math.abs(j - arr[i])]  = true;
+                    dp[i][Math.abs(j + arr[i])]  = true;
                 }
             }
+            dp[i][arr[i]] = true;
         }
 
-        for(int n : arr){
-            if(dp[N][n]) System.out.print("Y ");
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i =0; i<M;i++){
+            int a = Integer.parseInt(st.nextToken());
+            if(a > sum) {
+                System.out.print("N ");
+                continue;
+            }
+            if(dp[N-1][a]) System.out.print("Y ");
             else System.out.print("N ");
         }
+
 
     }
 }
