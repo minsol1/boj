13356c336090556ad1a7e4dce18c64bf2632c113
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-09-26 22:42:07 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,12 +1,10 @@
-select c.CAR_ID, c.CAR_TYPE ,round((DAILY_FEE*30)*(100-p.DISCOUNT_RATE)*0.01) FEE
-from CAR_RENTAL_COMPANY_CAR c 
-left join(select CAR_TYPE, DISCOUNT_RATE
-        from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-        where DURATION_TYPE = '30일 이상')p
+select c.CAR_ID,p.CAR_TYPE, c.DAILY_FEE*30*0.01*(100-ifnull(substring_index(p.DISCOUNT_RATE,'%',1),0)) as FEE
+from CAR_RENTAL_COMPANY_CAR c left join CAR_RENTAL_COMPANY_DISCOUNT_PLAN p
 on c.CAR_TYPE = p.CAR_TYPE
-where (c.CAR_TYPE = '세단' or c.CAR_TYPE = 'SUV' )
-        and c.CAR_ID not in (select CAR_ID
-                   from CAR_RENTAL_COMPANY_RENTAL_HISTORY h
-                   where  START_DATE <= '2022-11-30' and END_DATE >= '2022-11-01')
-    and ((DAILY_FEE*30)*(100-p.DISCOUNT_RATE)*0.01) >=500000 and ((DAILY_FEE*30)*(100-p.DISCOUNT_RATE)*0.01)<2000000
-order by FEE desc, c.CAR_TYPE,c.CAR_ID desc
+where c.CAR_ID not in (select CAR_ID
+                      from CAR_RENTAL_COMPANY_RENTAL_HISTORY h
+                       where h.START_DATE <="2022-11-30" and h.END_DATE >="2022-11-01"
+                      )
+                      and DURATION_TYPE = "30일 이상"
+                      and c.DAILY_FEE*30*0.01*(100-ifnull(substring_index(p.DISCOUNT_RATE,'%',1),0)) >=500000 and c.DAILY_FEE*30*0.01*(100-ifnull(substring_index(p.DISCOUNT_RATE,'%',1),0))  < 2000000 
+order by c.DAILY_FEE*30*0.01*(100-ifnull(substring_index(p.DISCOUNT_RATE,'%',1),0))  desc , p.CAR_TYPE,c.CAR_ID desc
