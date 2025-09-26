6a568dd0b99ee2b8f426088bf3eeaa726209824a
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-09-26 14:15:21 +0900

    [Silver III] Title: N과 M (8), Time: 104 ms, Memory: 17300 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -1,47 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	public static int N,M;
-	public static int[] arr;
-	public static StringBuilder sb;
-	
-	public static void dfs(int idx, int cnt, String temp) {
-		if(cnt==M) {
-			sb.append(temp).append("\n");
-			return;
-		}
-		
-		for(int i = idx; i<N;i++) {
-			dfs(i, cnt+1,temp+arr[i]+" ");
-		}
-	}
-	
+    public static int N,M;
+    public static int[] arr;
+    public static StringBuilder sb;
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		arr = new int[N];
-		sb= new StringBuilder();
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0;i<N;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(arr);
-		
-		dfs(0,0,"");
-		
-		System.out.println(sb);
+    public static void dfs(int idx, int cnt, int[] res){
+        if(cnt == M){
+            for(int num : res){
+                sb.append(num).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
 
-	}
+        for(int i =idx; i<N ; i++){
+            res[cnt] = arr[i];
+            dfs(i,cnt+1, res);
+        }
+    }
 
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i =0; i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs(0,0,new int[M]);
+
+        System.out.println(sb);
+
+    }
 }
