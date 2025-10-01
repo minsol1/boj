ba115c1b5b15d2b42df6f7d2227973016da4629b
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-10-01 22:49:30 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 55.06 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int[] solution(String[] gems) {
+        int N = gems.length;
+        int cnt = 0; 
+        int min = N;
+        HashSet<String> hs = new HashSet<>();
+        HashMap<String,Integer> hm = new HashMap<>();
+        int[] res = new int[]{1,N};
+        
+        for(int i =0; i< N ;i++){
+            hs.add(gems[i]);
+        }
+        cnt = hs.size();
+        
+        int l = 0; 
+        int r = 0; 
+        
+        while(l <= r){
+            if(hm.size() == cnt){
+                hm.put(gems[l], hm.getOrDefault(gems[l],1)-1);
+                if(hm.get(gems[l])== 0 ) hm.remove(gems[l]);
+                l++;
+                
+                if(r -l +1 < min){
+                    min = r -l +1;
+                    res = new int[]{l, r};
+                }
+                
+            }
+            else{
+                if(r >= N) break;
+                hm.put(gems[r], hm.getOrDefault(gems[r],0)+1);
+                r++;
+            }
+            
+        }
+        
+        return res;
+    }
+}
