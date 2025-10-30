4d6b16caeca9c5c30a2f972007f95be78f0225ad
minsol1 <81753858+minsol1@users.noreply.github.com>
2025-10-30 09:47:10 +0900

    [level 2] Title: 자동차 평균 대여 기간 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/157342. 자동차 평균 대여 기간 구하기/자동차 평균 대여 기간 구하기.sql
+++ 프로그래머스/2/157342. 자동차 평균 대여 기간 구하기/자동차 평균 대여 기간 구하기.sql
@@ -1,6 +1,6 @@
 -- 코드를 입력하세요
-SELECT  CAR_ID, round( avg(datediff( END_DATE,START_DATE))+1,1) AVERAGE_DURATION
+SELECT CAR_ID, round(avg(datediff(END_DATE, START_DATE)+1), 1) AVERAGE_DURATION
 from CAR_RENTAL_COMPANY_RENTAL_HISTORY
 group by CAR_ID
-having avg(datediff( END_DATE,START_DATE))+1>=7
+having avg(datediff(END_DATE, START_DATE)+1) >=7
 order by AVERAGE_DURATION desc, CAR_ID desc
