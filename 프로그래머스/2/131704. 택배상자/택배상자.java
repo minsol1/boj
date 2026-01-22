60c0f14b66a4c8eb0ef8b02009a4f127dab07bfc
minsol1 <81753858+minsol1@users.noreply.github.com>
2026-01-22 15:16:06 +0900

    [level 2] Title: 택배상자, Time: 194.07 ms, Memory: 143 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] order) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        int idx = 0; 
+        
+        for(int i = 1; i <= order.length; i++){
+            stack.add(i);
+            
+            while(!stack.isEmpty() && stack.peek() == order[idx]){
+                    int n = stack.pop();
+                    answer++;
+                    idx++;
+            }
+        }
+        return answer;
+    }
+}
