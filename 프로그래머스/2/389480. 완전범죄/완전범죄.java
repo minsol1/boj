2171c892a694a1226aa3be9d7873ffa549b366e0
minsol1 <81753858+minsol1@users.noreply.github.com>
2026-01-23 17:06:18 +0900

    [level 2] Title: 완전범죄, Time: 4056.00 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -1,92 +1,54 @@
-// import java.util.*;
-
-// class Solution {
-    
-//     public static int N, M;
-//     public static int[][] arr;
-//     public static int res;
-    
-//     public static void dfs(int A,int B,int idx){
-//         if(A >= N || B >= M) return;
-//         if(res <= A) return;
-        
-//         if(idx == arr.length) {
-//             res = res < A ? res : A;
-//             return;
-//         }
-        
-//         dfs(A, B + arr[idx][1] , idx + 1);
-//         dfs(A + arr[idx][0], B , idx + 1);
-//     }
-//     public int solution(int[][] info, int n, int m) {
-//         res = Integer.MAX_VALUE;
-//         arr = info;
-//         N = n;
-//         M = m;
-        
-//         Arrays.sort(arr, (a,b) -> b[1] - a[1]);
-        
-//         dfs(0,0,0);
-        
-//         if(res == Integer.MAX_VALUE) res = -1;
-//         return res;
-//     }
-// }
-
-
 import java.util.*;
-
 class Node implements Comparable<Node>{
-    int a;
-    int b;
+    int A;
+    int B;
     
-    public Node(int a , int b){
-        this.a = a;
-        this.b = b;
+    public Node (int A, int B){
+        this.A = A;
+        this.B = B;
     }
     
     public int compareTo(Node o){
-        if(a == o.a){
-            return o.b -b;
+        if(this.A == o.A){
+            return o.B - this.B;
         }
-        return o.a - a;
+        else return o.A - this.A;
     }
+    
 }
+
 class Solution {
     
-    public static int res;
-    public static int N,M;
+    public static int N, M;
     public static Node[] arr;
+    public static int res;
     
-    public static void dfs(int idx, int A, int B){
+    public static void dfs(int A,int B,int idx){
         if(A >= N || B >= M) return;
-        if(A >= res) return;
+        if(res <= A) return;
         
-        if(idx == arr.length){
-            res = Math.min(A, res);
+        if(idx == arr.length) {
+            res = res < A ? res : A;
             return;
         }
         
-        dfs(idx+1, A,B+ arr[idx].b);
-        dfs(idx+1, A+arr[idx].a,B);
-        
+        dfs(A, B + arr[idx].B , idx + 1);
+        dfs(A + arr[idx].A , B , idx + 1);
     }
-    
     public int solution(int[][] info, int n, int m) {
         res = Integer.MAX_VALUE;
         arr = new Node[info.length];
         N = n;
         M = m;
         
-        for(int i =0; i< info.length; i++){
+        for(int i = 0; i< info.length ; i++){
             arr[i] = new Node(info[i][0],info[i][1]);
         }
-        
         Arrays.sort(arr);
         
         dfs(0,0,0);
         
-        if( res == Integer.MAX_VALUE) res = -1;
+        if(res == Integer.MAX_VALUE) res = -1;
         return res;
     }
 }
