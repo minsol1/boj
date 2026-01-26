ed2ec4620a2d9054affd4f45499dfa8e7508c347
minsol1 <81753858+minsol1@users.noreply.github.com>
2026-01-26 17:33:45 +0900

    [level 2] Title: 완전범죄, Time: 0.29 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -1,54 +1,38 @@
 import java.util.*;
-class Node implements Comparable<Node>{
-    int A;
-    int B;
-    
-    public Node (int A, int B){
-        this.A = A;
-        this.B = B;
-    }
-    
-    public int compareTo(Node o){
-        if(this.A == o.A){
-            return o.B - this.B;
-        }
-        else return o.A - this.A;
-    }
-    
-}
 
 class Solution {
-    
-    public static int N, M;
-    public static Node[] arr;
-    public static int res;
-    
-    public static void dfs(int A,int B,int idx){
-        if(A >= N || B >= M) return;
-        if(res <= A) return;
-        
-        if(idx == arr.length) {
-            res = res < A ? res : A;
-            return;
-        }
-        
-        dfs(A, B + arr[idx].B , idx + 1);
-        dfs(A + arr[idx].A , B , idx + 1);
-    }
     public int solution(int[][] info, int n, int m) {
-        res = Integer.MAX_VALUE;
-        arr = new Node[info.length];
-        N = n;
-        M = m;
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0; 
         
-        for(int i = 0; i< info.length ; i++){
-            arr[i] = new Node(info[i][0],info[i][1]);
+        for(int i =0; i< info.length; i++){
+            int A = info[i][0];
+            int B = info[i][1];
+            int[] newdp =new int[n];
+            Arrays.fill(newdp, Integer.MAX_VALUE);
+            
+            for(int j = 0; j<n;j++){
+                
+                if(dp[j] == Integer.MAX_VALUE) continue;
+                
+                if(j + A < n ){
+                    newdp[j+A] = Math.min(newdp[j+A], dp[j]);
+                }
+                
+                if(dp[j] + B < m){
+                    newdp[j] = Math.min(dp[j] + B, newdp[j]);
+                }
+            }
+            
+            dp = newdp;
         }
-        Arrays.sort(arr);
         
-        dfs(0,0,0);
+        for(int i = 0; i< n; i++){
+            if(dp[i] != Integer.MAX_VALUE) return i;
+        }
+        return -1;
         
-        if(res == Integer.MAX_VALUE) res = -1;
-        return res;
+        
     }
 }
