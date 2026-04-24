7e5ffee87b3e920da953315a457147aae7fd15cb
minsol1 <81753858+minsol1@users.noreply.github.com>
2026-04-24 13:54:18 +0900

    [level 3] Title: 단어 변환, Time: 0.37 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,50 +1,68 @@
-import java.io.*;
 import java.util.*;
 
-class Node{
-    char[] str;
-    int num;
-    
-    public Node(char[] str, int num){
-        this.str = str;
-        this.num = num;
-    }
-}
 class Solution {
-
-    public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        HashMap<String,Integer> hs = new HashMap<>();
-        for(String str : words) hs.put(str,0);
+    
+    public static String b, t;
+    public static String[] word;
+    public static HashSet<String> hs;
+    public static HashMap<String,Integer> visited;
+    
+    public static int dif(String s, String c){
         
-        Queue<Node> q = new ArrayDeque<>();
-        q.add(new Node(begin.toCharArray(),0));
+        int cnt = 0; 
+        for(int i =0; i< s.length(); i++){
+            if(s.charAt(i) != c.charAt(i)){
+                cnt++;
+            }
+            if(cnt > 1) return cnt;
+        }
+        // System.out.println(s+" "+c+" "+ cnt );
+        
+        return cnt;
+        
+    }
+    
+    public static void bfs(String s){
+        visited.put(s,0);
+        Queue<String> q = new ArrayDeque<>();
+        q.add(s);
+        
         while(!q.isEmpty()){
-            Node now = q.poll();
             
-            char[] str = now.str;
-            for(int i =0; i < str.length;i++){
-                for(int j = 0; j <26; j++){
-                    char t = str[i];
-                    char c =(char) ('a' + j);
-                    if(c > 'z' || c < 'a') continue;
-                    str[i] = c;
-                    
-                    String tem = new String(str);
-                    System.out.println(tem);
-                    if(hs.containsKey(tem) && hs.get(tem)==0){
-                        q.add(new Node(tem.toCharArray(), now.num+1));
-                        hs.put(tem,now.num+1);
-                        System.out.println("~~~~~"+tem);
-                    }
-                    str[i] =t;
+            String now = q.poll();
+            
+            for(String nx : word){
+                if(visited.containsKey(nx) || 1 < dif(now, nx)){
+                    continue;
                 }
                 
+                visited.put(nx, visited.get(now)+1);
+                q.add(nx);
             }
         }
-        if(hs.containsKey(target)){
-            answer = hs.get(target);
+        
+        
+    }
+    
+    public int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        hs = new HashSet<>();
+        visited = new HashMap<>();
+        b = begin;
+        t = target;
+        word = words;
+        
+        for(String str : words){
+            hs.add(str);
         }
+        
+        bfs(begin);
+        
+        if(visited.containsKey(t)){
+            answer = visited.get(t);
+        }
+        
+        
         return answer;
     }
 }
