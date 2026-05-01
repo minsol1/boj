69226a48891e86b906bdd151ab90f7a9452be479
minsol1 <81753858+minsol1@users.noreply.github.com>
2026-05-01 15:35:36 +0900

    [level 3] Title: 네트워크, Time: 0.63 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,37 +1,60 @@
 import java.util.*;
 class Solution {
-    public static int N;
     public static boolean[] visited;
-    public static int[][] arr;
+    public static int N ; 
+    public static ArrayList<Integer>[] arr;
+    public static int res; 
+    
     
     public static void bfs(int s){
         Queue<Integer> q = new ArrayDeque<>();
-        visited[s] = true;
         q.add(s);
         
         while(!q.isEmpty()){
             int now = q.poll();
             
-            for(int i = 0; i<N;i++){
-                if(arr[now][i] == 1 && !visited[i]){
-                    q.add(i);
-                    visited[i] = true;
+            for(int i =0; i< arr[now].size(); i++ ){
+                int nx = arr[now].get(i);
+                
+                if(!visited[nx]){
+                    visited[nx] = true;
+                    q.add(nx);
                 }
             }
         }
+        
+        
+        
     }
     
+    
     public int solution(int n, int[][] computers) {
-        int answer = 0;
+        res = 0;
+        visited = new boolean[n];
         N = n;
-        visited = new boolean[N];
-        arr = computers;
-        for(int i =0;i<N;i++){
-            if(!visited[i]){
-                bfs(i);
-                answer++;
+        arr = new ArrayList[n];
+        for(int i = 0 ; i <N; i++){
+            arr[i] = new ArrayList<>();
+        }
+        
+        for(int i =0; i< computers.length ; i++){
+            
+            for(int j =0; j< computers[i].length; j++){
+                if(computers[i][j] == 1){
+                    arr[i].add(j);
+                    arr[j].add(i);
+                }
             }
         }
-        return answer;
+        
+        for(int i =0; i< N ; i++ ){
+            if(!visited[i]){
+                bfs(i);
+                res ++;
+                
+            }
+        }
+        
+        return res;
     }
 }
