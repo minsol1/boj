6fa0bbda36701b1f242bddd48c5efd5831a029e0
minsol1 <81753858+minsol1@users.noreply.github.com>
2026-05-01 13:32:16 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,14 +2,17 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int res = 0;
+        int cnt = 0;
         
-        while(a!=b){
-            res++;
+        while(a !=b){
             a = (a+1)/2;
             b = (b+1)/2;
-            
+            cnt++;
         }
-        return res;
+        
+        return cnt;
+        
+        
+        
     }
 }
