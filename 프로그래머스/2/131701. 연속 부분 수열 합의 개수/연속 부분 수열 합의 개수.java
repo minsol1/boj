629744b25ff019ac57c0eef64eefb5e36e90cfc2
minsol1 <81753858+minsol1@users.noreply.github.com>
2026-05-22 13:34:13 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 65.13 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,18 +1,32 @@
 import java.util.*;
+// import java.io.*;
 
 class Solution {
     public int solution(int[] elements) {
-        HashSet<Integer> hs = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
         int N = elements.length;
         
-        for(int i =0; i< N; i++){
+        for(int i = 1; i <=N ; i++){
             int sum =0;
-            for(int j = 0; j < N ; j++){
-                sum += elements[(i+j)%N];
-                hs.add(sum);
+            
+            for(int j = 0; j<i; j++){
+                sum+=elements[j];
             }
+            set.add(sum);
+            
+            // System.out.print(i+ ": "+sum+" ");
+            
+            for(int j =0; j< N ; j++){
+                int e = (j + i)%N;
+                sum-=elements[j];
+                sum+=elements[e];
+                
+                set.add(sum);
+                // System.out.print(sum+" ");
+            }
+            
+            // System.out.println("");
         }
-        
-        return hs.size();
+        return set.size();
     }
 }
