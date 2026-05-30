b46551d678521cb8da1aaf4bd9268a3d930ea0c1
minsol1 <81753858+minsol1@users.noreply.github.com>
2026-05-30 14:19:08 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,34 +1,32 @@
 import java.util.*;
 
+
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int N = speeds.length;
-        int[] need = new int[N];
-        int[] res;
-        ArrayList<Integer> arr = new ArrayList<>();
+        ArrayList<Integer> res = new ArrayList<>();
         
-        for(int i = 0; i< N ; i++){
-            need[i] = (100 - progresses[i]) /speeds[i];
-            if((100 - progresses[i]) % speeds[i] != 0 ) need[i]++;
-        }
-        
-        int idx = -1; 
         int day = 0;
         
-        for(int i =0; i< N; i++){
-            if(need[i] > day){
-                day = need[i];
-                idx++;
-                arr.add(1);
+        for(int i =0; i< progresses.length; i++){
+            if(progresses[i] + day*speeds[i] >= 100) continue;
+            
+            day = ( 100 - progresses[i] ) / speeds[i] ;
+            if(( 100 - progresses[i] ) % speeds[i]  >0) day++;
+            // System.out.println(day);
+            
+            int cnt = 1;
+            for(int j =i+1; j < progresses.length ; j++){
+                if(progresses[j] + day*speeds[j] >= 100) cnt++;
+                else break;
             }
-            else{
-                arr.set(idx, arr.get(idx) +1);
-            }
+            
+            res.add(cnt);            
         }
-        res = new int[arr.size()];
-        for(int i =0;i< arr.size(); i++){
-            res[i] = arr.get(i);
+        
+        int[] answer = new int[res.size()];
+        for(int i =0; i< res.size(); i++){
+            answer[i] = res.get(i);
         }
-        return res;
+        return answer;
     }
 }
