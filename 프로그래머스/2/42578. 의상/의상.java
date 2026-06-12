c137e4effe8037eb7c4d28fc80a982b402e79fbc
minsol1 <81753858+minsol1@users.noreply.github.com>
2026-06-12 10:50:32 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,25 +1,28 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        HashMap<String, Integer> hm = new HashMap<>();
+        HashMap<String, HashSet<String>> hm = new HashMap<>();
+        HashSet<String> hs = new HashSet<>();
         
-        for(int i =0; i< clothes.length; i++){
+        for(int i =0; i< clothes.length ; i++){
+            String name = clothes[i][0];
             String type = clothes[i][1];
             
-            if(hm.containsKey(type)){
-                hm.put(type, hm.get(type)+1);
+            if(!hm.containsKey(type)){
+                hm.put(type, new HashSet<String>());
+                hs.add(type);
             }
-            else{
-                hm.put(type,1);
-            }
+            hm.get(type).add(name);
         }
         
-        for(int n : hm.values()){
-            answer*=(n+1);
+        for(String type : hs){
+            answer *= (hm.get(type).size()+1);
         }
-        answer--;
+        answer --;
+        
         return answer;
     }
 }
