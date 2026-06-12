ef2f9c6b88e5545ab15d31ffcc2082a612745d6e
minsol1 <81753858+minsol1@users.noreply.github.com>
2026-06-12 11:19:18 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 32.30 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,54 +1,56 @@
-import java.util.*;
 class Solution {
-    public static int[] time;
     public static int[] diff;
+    public static int[] time;
     public static long lim;
     public static int N;
     
-    public static boolean isPossible(int lev){
-        int time_prev = 0;
-        long sum = 0;
+    public static boolean get_time(int level){
+        long total = 0;
         
-        for(int i = 0; i<N;i++){
-            if(lev >= diff[i]){
-                sum+= time[i];
+        for(int i =0; i<N; i++){
+            int d = diff[i]; // 난이도 
+            int cur = time[i];// 현재 퍼즐 시간
+            int prev = 0; // 이전 퍼즐 시간 
+            if(i >0){
+                prev = time[i-1];
+            }
+            
+            if(d <= level){
+                total+= cur;
             }
             else{
-                sum= sum + (diff[i]-lev)*(time[i]+time_prev) + time[i];
+                total += ((d-level)*(cur+prev) + cur);
             }
-            time_prev = time[i];
-            if(sum > lim) return false;
+            
+            if(total >lim) return false;
         }
-        System.out.println(lev+" "+ sum);
+        
         return true;
         
     }
+    
     public int solution(int[] diffs, int[] times, long limit) {
+        int answer = 0;
         diff = diffs;
         time = times;
         lim = limit;
-        N = times.length;
-        int r = 0;
-        
-        for(int i =0; i < N; i++){
-            r = Math.max(r, diffs[i]);
-        }
+        N = diffs.length;
         
         int l = 1;
-        int res = r;
+        int r = 100000;
         
-        while(l<r){
-            int m = l + (r-l)/2;
+        while(l <= r){
+            int m = l + (r - l)/2;
             
-            if(isPossible(m)){
-                r = m;
-                res = m;
+            if(get_time(m)){
+                answer = m;
+                r = m -1;
             }
             else{
                 l = m+1;
             }
         }
         
-        return res;
+        return answer;
     }
 }
