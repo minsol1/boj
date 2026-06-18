1debf63713ac90b53d0865a361e9b488be5cab64
minsol1 <81753858+minsol1@users.noreply.github.com>
2026-06-18 13:46:36 +0900

    [level 2] Title: 테이블 해시 함수, Time: 5.76 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -1,45 +1,22 @@
+import java.io.*;
 import java.util.*;
 
-class Data implements Comparable<Data>{
-    int[] row;
-    
-    public static int col;
-    
-    public Data(int[] row){
-        this.row = row;
-    }
-    
-    public int compareTo(Data o){
-        if(o.row[col-1] == this.row[col-1]){
-            return o.row[0] - this.row[0];
-        }
-        else{
-            return this.row[col-1] - o.row[col-1];
-        }
-    }
-    
-}
 class Solution {
     public int solution(int[][] data, int col, int row_begin, int row_end) {
-        ArrayList<Data> arr = new ArrayList<>();
-        int res = 0;
+        int answer = 0;
         
-        for(int i =0; i< data.length;i++){
-            arr.add(new Data(data[i]));
-        }
-        Data.col = col;
+        Arrays.sort(data, (a,b) -> a[col-1]== b[col-1]? b[0]-a[0] :a[col-1]- b[col-1]  );
         
-        Collections.sort(arr);
         
-        for(int i = row_begin-1 ; i <row_end ; i++){
-            int sum = 0;
-            int[] row = arr.get(i).row;
-            for(int j = 0; j< row.length; j++){
-                sum += row[j]%(i+1);
+        for(int i = row_begin-1 ; i< row_end; i++){
+            long sum = 0;
+            
+            for(int j =0; j< data[i].length; j++){
+                sum+= (data[i][j]%(i+1));
             }
-            res ^= sum;
+            
+            answer ^=sum;
         }
-        
-        return res;
+        return answer;
     }
 }
