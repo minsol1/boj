61d09d0a19b501e1b4467b8b19faf9bbdcc9e760
minsol1 <81753858+minsol1@users.noreply.github.com>
2026-09-11 19:39:54 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,37 +1,12 @@
-# select history_ID, round( DAILY_FEE*(datediff(END_DATE, START_DATE) +1 ) * (1- 0.01 *ifnull(cast ( substring_index(DISCOUNT_RATE,'%',1) as signed),0) )  )fee
-# from CAR_RENTAL_COMPANY_RENTAL_HISTORY rh left join CAR_RENTAL_COMPANY_CAR c
-# on rh.CAR_ID = c.CAR_ID
-# left join CAR_RENTAL_COMPANY_DISCOUNT_PLAN dp
-# on case when datediff(END_DATE, START_DATE) +1 >= 90 then "90일 이상"
-#         when datediff(END_DATE, START_DATE) +1 >= 30 then "30일 이상"
-#         when datediff(END_DATE, START_DATE) +1 >= 70 then "70일 이상"
-#         end
-#         = dp.DURATION_TYPE and dp.CAR_TYPE = c.CAR_TYPE
-# where c.CAR_TYPE ='트럭'
-# order by fee desc, history_ID desc
 
+SELECT HISTORY_ID, DAILY_FEE*day*(1 - 0.01* ifnull (discount_rate,0)) FEE
+from ( select h.CAR_ID, HISTORY_ID, datediff(END_DATE,START_DATE)+1 day, DAILY_FEE,CAR_TYPE
+      from CAR_RENTAL_COMPANY_RENTAL_HISTORY h left join CAR_RENTAL_COMPANY_CAR c 
+      on h.CAR_ID = c.CAR_ID
+      where CAR_TYPE = '트럭'
+     ) d
+     left join CAR_RENTAL_COMPANY_DISCOUNT_PLAN p
+     on d.CAR_TYPE = p.CAR_TYPE and substring_index(DURATION_TYPE,"일",1) <= day
 
-
-
-
-
-
-
-
-
-
-SELECT 
-    h.history_id,
-    ROUND(((DATEDIFF(h.end_date, h.start_date) + 1) * c.daily_fee) *
-    (1 - (IFNULL(p.discount_rate, 0) / 100)), 0) AS fee
-FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY h
-LEFT JOIN CAR_RENTAL_COMPANY_CAR c
-    ON h.car_id = c.car_id
-LEFT JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN p
-    ON CASE WHEN (DATEDIFF(h.end_date, h.start_date) + 1) >= 90 THEN '90일 이상'
-            WHEN (DATEDIFF(h.end_date, h.start_date) + 1) >= 30 THEN '30일 이상'
-            WHEN (DATEDIFF(h.end_date, h.start_date) + 1) >= 7 THEN '7일 이상' END
-        = p.duration_type
-    AND c.car_type = p.car_type
-WHERE c.car_type = "트럭"
-ORDER BY fee DESC, history_id DESC
+group by HISTORY_ID
+order by fee desc,  HISTORY_ID desc
